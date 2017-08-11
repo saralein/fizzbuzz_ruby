da2b2fd678ad10b1986cd82a327f93a3c794a89c
saralein <sara.m.trujillo@gmail.com>
2017-08-11 16:29:04 -0500

    Add additional numbers for three and five divisibility tests.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,11 +3,23 @@ require_relative '../lib/fizzbuzz.rb'
 describe 'fizzbuzz' do
   it 'prints fizz if number is divisible by three' do
     expect(STDOUT).to receive(:puts).with('fizz')
-    fizzbuzz(Array.new(1){3})
+    fizzbuzz([3])
+
+    expect(STDOUT).to receive(:puts).with('fizz')
+    fizzbuzz([9])
+
+    expect(STDOUT).to receive(:puts).twice.with('fizz')
+    fizzbuzz([6, 12])
   end
 
   it 'prints buzz if number is divisible by five' do
     expect(STDOUT).to receive(:puts).with('buzz')
-    fizzbuzz(Array.new(1){5})
+    fizzbuzz([5])
+
+    expect(STDOUT).to receive(:puts).with('buzz')
+    fizzbuzz([20])
+
+    expect(STDOUT).to receive(:puts).twice.with('buzz')
+    fizzbuzz([10, 25])
   end
 end
