1a9186d7bc2dc9d34fe0bc95438ea198a8f7a470
saralein <sara.m.trujillo@gmail.com>
2017-08-11 18:13:12 -0500

    Refactor fizzbuzz. Rename original fizzbuzz to fizzbuzz_original (kept in place for demo).

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,19 @@
+#refactor
 def fizzbuzz(numbers)
+  numbers.each do |number|
+    output = ''
+    if (number % 3 == 0)
+      output += 'fizz'
+    end
+    if (number % 5 == 0)
+      output += 'buzz'
+    end
+    puts output == '' ? number : output
+  end
+end
+
+# original
+def fizzbuzz_original(numbers)
   numbers.each do |number|
     if (number % 3 == 0 and number % 5 == 0)
       puts 'fizzbuzz'
