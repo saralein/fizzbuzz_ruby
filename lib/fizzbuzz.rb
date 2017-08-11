eb6956a6b84ec580b73c3fb022878e94d7e3bf6b
saralein <sara.m.trujillo@gmail.com>
2017-08-11 16:22:25 -0500

    Add code and tests for numbesr divisible by five.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,8 @@ def fizzbuzz(numbers)
   numbers.each do |number|
     if(number % 3 == 0)
       puts 'fizz'
+    elsif(number % 5 ==0)
+      puts 'buzz'
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,9 @@ describe 'fizzbuzz' do
     expect(STDOUT).to receive(:puts).with('fizz')
     fizzbuzz(Array.new(1){3})
   end
+
+  it 'prints buzz if number is divisible by five' do
+    expect(STDOUT).to receive(:puts).with('buzz')
+    fizzbuzz(Array.new(1){5})
+  end
 end
-
-
